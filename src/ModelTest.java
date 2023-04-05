0ccc87d8dcec6b02e82745af503a0b856f53af51
David Vu <vudavidwork5564@gmail.com>
2023-04-04 23:15:58 -0700

    Added some comments for notes and still testing how branching works.

--- src/ModelTest.java
+++ src/ModelTest.java
@@ -2,5 +2,18 @@ public class ModelTest
 {
 
     // I don't know how collab github stuff work!!!!!
+    // Testing branch things right now
+
+
+    // Mancalla, 2 goal end, 6 x 2 circles, each can hold 4 marbles max
+    // A1, A2, A3, A4, A5, A6, B1, B2, B3, B4, B5, B6 are the circles respectively
+
+    // Maybe for data structure a HashMap of Arraylists? Where each "A1, A2" can be the key and the value is the ArrayList holding the amount of marbles. It could just be an int too.
+
+    public Model()
+    {
+
+    }
+
 
 }
