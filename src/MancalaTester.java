46363a4f3cc53b7c07fefb774193e7e7dc237b7f
David Vu <vudavidwork5564@gmail.com>
2023-04-10 18:21:34 -0700

    Added Mancala MVC. Still adding it to my branch for now it is spooky.

---
+++ src/MancalaTester.java
@@ -0,0 +1,8 @@
+public class MancalaTester
+{
+    public static void main(String[] args)
+    {
+        MancalaModel model = new MancalaModel();
+        MancalaView view = new MancalaView(model);
+    }
+}
